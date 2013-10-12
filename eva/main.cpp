d9a68b1980ff60c4faad17f2447e574045a6c3cd
Evelina <Eva@pc-194-62.customer.sugar.crm.lan>
2013-10-12 12:42:21 +0100

    Initial Commit

---
+++ eva/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  eva
+//
+//  Created by Evelina on 12/10/2013.
+//  Copyright (c) 2013 Evelina. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
